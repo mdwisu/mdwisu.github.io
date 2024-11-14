e50e8ae11105e1f7fb5d1082e0bc2c037ed1e664
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-11-14 18:41:36 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Sesuaikan dengan nama repository Anda
+  basePath: "/mdwisu.github.io",
+  assetPrefix: "/mdwisu.github.io",
 };
 
 export default nextConfig;
