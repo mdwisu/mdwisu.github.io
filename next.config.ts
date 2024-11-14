72d89cd6e6343ef5613da52ac2f602f57b18e84f
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-11-14 21:32:34 +0700

    updatee

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? "/https://mdwisu.github.io" : "",
-  basePath: isProd ? "/https://mdwisu.github.io" : "",
   output: "export",
 };
 
