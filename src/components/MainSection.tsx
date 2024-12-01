f4610f00d286cfc7bfae0b6ce615883942376180
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-01 23:32:18 +0700

    update main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,11 +1,38 @@
 "use client";
 // import { useCursor } from "@/components/cursor/CursorContext";
-import React from "react";
 import { motion } from "framer-motion";
 import Magnet from "@/components/Magnet";
+import { useEffect, useState } from "react";
 
 export default function MainSection() {
   // const { setCursorType } = useCursor();
+  const [textIndex, setTextIndex] = useState(0);
+  const text = "Hi, I'm Muhammad Dwi Susanto!";
+  const typingSpeed = 150;
+  const delayAfterTyping = 2000;
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    let resetTimeoutId: NodeJS.Timeout;
+
+    // Typing effect
+    if (textIndex < text.length) {
+      intervalId = setInterval(() => {
+        setTextIndex((prevIndex) => prevIndex + 1);
+      }, typingSpeed);
+    } else {
+      // Once typing is complete, reset after delay
+      resetTimeoutId = setTimeout(() => {
+        setTextIndex(0); // Reset the typing index to start from the beginning
+      }, delayAfterTyping);
+    }
+
+    // Clear the intervals and timeouts on cleanup
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(resetTimeoutId);
+    };
+  }, [textIndex]);
+
   return (
     <div className="min-h-screen bg-gray-800">
       <Magnet strength={50}>
@@ -24,7 +51,7 @@ export default function MainSection() {
               className="text-center"
             >
               <h1 className="text-4xl font-bold text-gray-100">
-                Hi, I&apos;m Muhammad Dwi Susanto!
+                {text.substring(0, textIndex)}
               </h1>
               <p className="mt-4 text-lg text-gray-300">
                 A full-stack developer passionate about creating web solutions.
